19b35dc94686f2efe4d408f532b302cf834f68d8
HokagyDev <l79142829@gmail.com>
2026-07-31 09:07:19 +0300

    Update SelectionManager.java

--- src/main/java/dev/hokagy/novacassino/manager/SelectionManager.java
+++ src/main/java/dev/hokagy/novacassino/manager/SelectionManager.java
@@ -1,7 +1,6 @@
 package dev.hokagy.novacassino.manager;
 
 import org.bukkit.Location;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -29,19 +28,5 @@ public class SelectionManager {
 
     public boolean hasSelection(UUID playerId) {
         return pos1Map.containsKey(playerId) && pos2Map.containsKey(playerId);
-    }
-
-    public Location getMinCorner(UUID playerId) {
-        Location p1 = pos1Map.get(playerId);
-        Location p2 = pos2Map.get(playerId);
-        if (p1 == null || p2 == null || p1.getWorld() == null || !p1.getWorld().equals(p2.getWorld())) {
-            return null;
-        }
-
-        double minX = Math.min(p1.getX(), p2.getX());
-        double minY = Math.min(p1.getY(), p2.getY());
-        double minZ = Math.min(p1.getZ(), p2.getZ());
-
-        return new Location(p1.getWorld(), minX, minY, minZ);
     }
 }
